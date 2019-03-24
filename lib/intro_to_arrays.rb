6d1b6f3fd6b03da6c59b3c1ee915dd3e4f3f5d13
Susan Tan <ssusantan9@gmail.com>
2019-03-24 23:11:13 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  arr = []
+  return arr
+end
+
+def array_with_two_elements
+  arr = [1, 2]
+  return arr
+end
+
+def first_element(arr)
+  return arr[0]
+end  
+
+def third_element(arr)
+  return arr[2]
+end
+
+def last_element(arr)
+  return arr[-1]
+end
+
+def first_element_with_array_methods(arr)
+  return arr.first
+end
+
+def last_element_with_array_methods(arr)
+  return arr.last
+end
+
+def length_of_array(arr)
+  return arr.size
+end
